2a19a853c61d10f7a701820364c0e8c1a48b3ad6
Salomon_PR <starwoker1@gmail.com>
2024-07-19 13:37:21 -0500

    fix: con los cambios de lfor

--- src/test/java/com/magento/steps/FacturaSteps.java
+++ src/test/java/com/magento/steps/FacturaSteps.java
@@ -1,34 +1,47 @@
 package com.magento.steps;
 
-import com.magento.tests.FacturaTest;
+import com.magento.pages.ComprobanteFacPage;
+import com.magento.pages.HomePage;
+import com.magento.pages.LoginPage;
+import com.magento.pages.RegisterPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class FacturaSteps {
- 
-    FacturaTest facturaTest;
+
+    protected static WebDriver driver;
+    public RegisterPage registerPage;
+    public LoginPage loginPage;
+    public HomePage homePage;
+    public ComprobanteFacPage comprobanteFacPage;
 
     public FacturaSteps() {
-        facturaTest = new FacturaTest();
+        driver= new EdgeDriver();//instanciando el objeto driver
+        homePage= new HomePage(driver);
+        registerPage=new RegisterPage(driver) ;
+        loginPage=new LoginPage(driver);
+        comprobanteFacPage=new ComprobanteFacPage(driver);
     }
     
     @Given("usuario logueado")
     public void usuario_logueado() {
-        facturaTest.loginPage.fillOutFormData("demo-pvlg", "demo123");
-        facturaTest.homePage.irFactura();
+        loginPage.fillOutFormData("demo-pvlg", "demo123");
+        homePage.irFactura();
     }
     @When("Se ingresan los datos de cliente e items")
     public void se_ingresan_los_datos_de_cliente_e_items() {
-        facturaTest.comprobanteFacPage.datosCliente();
-        facturaTest.comprobanteFacPage.agregarItemManual();
+         comprobanteFacPage.datosCliente();
+         comprobanteFacPage.agregarItemManual();
     }
     @When("Se presiona emitir")
     public void se_presiona_emitir() {
-        facturaTest.comprobanteFacPage.clickButtonEmitir();
+         comprobanteFacPage.clickButtonEmitir();
     }
     @Then("Aparece la vista del pdf de la factura emitida")
     public void aparece_la_vista_del_pdf_de_la_factura_emitida() {
-        facturaTest.comprobanteFacPage.validarDocumentoEmitido();
+         comprobanteFacPage.validarDocumentoEmitido();
     }
 }
